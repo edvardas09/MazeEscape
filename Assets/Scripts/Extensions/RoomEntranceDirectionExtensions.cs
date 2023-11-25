888c46d68735179f96ed82406f5ec5da4a361d10
edvardas099 <ydvardas09@gmail.com>
2023-11-26 01:03:20 +0200

    move extensions to new folder

---
+++ Assets/Scripts/Extensions/RoomEntranceDirectionExtensions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace MazeEscape.Room
+{
+    public static class RoomEntranceDirectionExtensions
+    {
+        public static RoomEntranceDirection GetOpposite(this RoomEntranceDirection direction)
+        {
+            return direction switch
+            {
+                RoomEntranceDirection.Top => RoomEntranceDirection.Bottom,
+                RoomEntranceDirection.Right => RoomEntranceDirection.Left,
+                RoomEntranceDirection.Bottom => RoomEntranceDirection.Top,
+                RoomEntranceDirection.Left => RoomEntranceDirection.Right,
+                _ => default,
+            };
+        }
+
+        public static Vector2Int GetVector(this RoomEntranceDirection direction)
+        {
+            return direction switch
+            {
+                RoomEntranceDirection.Top => Vector2Int.up,
+                RoomEntranceDirection.Right => Vector2Int.right,
+                RoomEntranceDirection.Bottom => Vector2Int.down,
+                RoomEntranceDirection.Left => Vector2Int.left,
+                _ => default,
+            };
+        }
+    }
+}
--- Assets/Scripts/Room/RoomEntranceDirection.cs
+++ Assets/Scripts/Room/RoomEntranceDirection.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace MazeEscape.Room
 {
     public enum RoomEntranceDirection
@@ -8,33 +6,5 @@ namespace MazeEscape.Room
         Right,
         Bottom,
         Left
-    }
-
-
-    public static class RoomEntranceDirectionExtensions
-    {
-        public static RoomEntranceDirection GetOpposite(this RoomEntranceDirection direction)
-        {
-            return direction switch
-            {
-                RoomEntranceDirection.Top => RoomEntranceDirection.Bottom,
-                RoomEntranceDirection.Right => RoomEntranceDirection.Left,
-                RoomEntranceDirection.Bottom => RoomEntranceDirection.Top,
-                RoomEntranceDirection.Left => RoomEntranceDirection.Right,
-                _ => default,
-            };
-        }
-
-        public static Vector2Int GetVector(this RoomEntranceDirection direction)
-        {
-            return direction switch
-            {
-                RoomEntranceDirection.Top => Vector2Int.up,
-                RoomEntranceDirection.Right => Vector2Int.right,
-                RoomEntranceDirection.Bottom => Vector2Int.down,
-                RoomEntranceDirection.Left => Vector2Int.left,
-                _ => default,
-            };
-        }
     }
 }
