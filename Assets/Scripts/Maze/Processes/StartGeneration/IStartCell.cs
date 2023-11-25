b7497c09921ddd5468cd6c3449cdd7b8f7053c03
edvardas099 <ydvardas09@gmail.com>
2023-11-26 00:43:55 +0200

    create start point generation

---
+++ Assets/Scripts/Maze/Processes/StartGeneration/IStartCell.cs
@@ -0,0 +1,4 @@
+public interface IStartCell
+{
+    public Cell GetStartCell();
+}
---
+++ Assets/Scripts/Maze/Processes/StartGeneration/StartGenerationSO.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MazeEscape.Maze
+{
+    [CreateAssetMenu(fileName = "StartGeneration", menuName = "Maze/Generation/StartGeneration")]
+    public class StartGenerationSO : GenerationProcessSO, IStartCell
+    {
+        [SerializeField] private Cell m_startCell;
+
+        public Cell GetStartCell()
+        {
+            return m_startCell;
+        }
+
+        public override void OnDrawGizmos()
+        {
+            if (m_startCell == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(new Vector3(m_startCell.Coords.x, 0, m_startCell.Coords.y), new Vector3(1, 1, 1));
+        }
+
+        protected override void OnGenerate()
+        {
+            var cellGeneration = GetProcess<ICellGeneration>();
+            var cells = cellGeneration.GetCells();
+
+            m_startCell = cells[Random.Range(0, cells.Count)];
+        }
+    }
+}
