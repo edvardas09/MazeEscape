185cb5e972456fd22a79ab917480addd9c9cd125
edvardas099 <ydvardas09@gmail.com>
2023-11-27 14:19:23 +0200

    update enemy generation to spawn multiple enemies

--- Assets/Scripts/GenerationProcesses/EnemyGeneration/EnemyGenerationSO.cs
+++ Assets/Scripts/GenerationProcesses/EnemyGeneration/EnemyGenerationSO.cs
@@ -1,5 +1,6 @@
 using MazeEscape.Gameplay.State;
 using MazeEscape.MazeGenerator;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MazeEscape.GenerationProcesses
@@ -8,6 +9,7 @@ namespace MazeEscape.GenerationProcesses
     public class EnemyGenerationSO : GenerationProcessSO
     {
         [SerializeField] private GameObject m_enemyPrefab;
+        [SerializeField] private int m_enemyCount = 2;
 
         private GameObject m_enemy;
 
@@ -18,21 +20,42 @@ namespace MazeEscape.GenerationProcesses
                 DestroyImmediate(m_enemy);
             }
 
-            if (!TryGetProcess<IFinishCell>(out var finishCellProcess))
+            if (!TryGetProcess<IStartCell>(out var startCellProcess))
             {
-                Debug.LogError("Finish cell not found");
+                Debug.LogError("Start cell not found");
                 return false;
             }
 
-            var finishCell = finishCellProcess.GetFinishCell();
-            m_enemy = Instantiate(m_enemyPrefab, finishCell.Coords.CellToWorldSpace(1), Quaternion.identity);
-            if (!m_enemy.TryGetComponent<StateMachine>(out var stateMachine))
+            if (!TryGetProcess<ICellGeneration>(out var cellGenerationProcess))
             {
-                Debug.LogError("Enemy prefab does not have a StateMachine component");
+                Debug.LogError("ICellGeneration not found");
                 return false;
             }
 
-            stateMachine.Initialize(m_mazeGenerator);
+            var startCell = startCellProcess.GetStartCell();
+            var cells = cellGenerationProcess.GetCells();
+            var availableCells = new List<Cell>(cells);
+            availableCells.Remove(startCell);
+
+            for (var i = 0; i < m_enemyCount; i++)
+            {
+                var randomCell = availableCells[Random.Range(0, availableCells.Count)];
+                m_enemy = Instantiate(m_enemyPrefab, randomCell.Coords.CellToWorldSpace(1), Quaternion.identity);
+                if (!m_enemy.TryGetComponent<StateMachine>(out var stateMachine))
+                {
+                    Debug.LogError("Enemy prefab does not have a StateMachine component");
+                    return false;
+                }
+
+                availableCells.Remove(randomCell);
+                stateMachine.Initialize(m_mazeGenerator);
+
+                if (availableCells.Count < 1)
+                {
+                    availableCells.AddRange(cells);
+                    availableCells.Remove(startCell);
+                }
+            }
 
             return true;
         }
